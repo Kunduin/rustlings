5d50ebf622a75a107168195392be52e5f9c5ee2f
Bay <15852158126@163.com>
2021-03-31 13:41:31 +0800

    commit 3.31

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -1,8 +1,6 @@
 // modules2.rs
 // Make me compile! Execute `rustlings hint modules2` for hints :)
 
-// I AM NOT DONE
-
 mod delicious_snacks {
     use self::fruits::PEAR as fruit;
     use self::veggies::CUCUMBER as veggie;
